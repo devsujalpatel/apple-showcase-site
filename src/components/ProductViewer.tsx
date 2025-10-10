7f5286c6c4cbf96eb10508a922366f667c5ec1f4
devsujalpatel <sujal7455@gmail.com>
2025-10-10 23:13:34 +0530

    Models jsx added

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -4,6 +4,7 @@ import { Box, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
 import React from "react";
+import { MacbookModel14 } from "./models/Macbook-14";
 
 export const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -59,11 +60,9 @@ export const ProductViewer = () => {
         id="canvas"
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
       >
-        <Box
-          position={[0, 0, 0]}
-          scale={scale * 10}
-          material-color={color}
-        ></Box>
+
+        <ambientLight intensity={1} />
+        <MacbookModel14 scale={scale} color={color} position={[0, 0, 0]} />
         <OrbitControls enableZoom={false} />
       </Canvas>
     </section>
