18551418142b68b0d2e7afe328a322bc9704329e
devsujalpatel <sujal7455@gmail.com>
2025-10-10 20:52:36 +0530

    Store Created

---
+++ src/components/ProductViewer.tsx
@@ -0,0 +1,58 @@
+"use client";
+import useMacbookStore from "@/store";
+import clsx from "clsx";
+import React from "react";
+
+export const ProductViewer = () => {
+  const { color, scale, setColor, setScale } = useMacbookStore();
+
+  return (
+    <section id="product-viewer">
+      <h2>Take a closer look.</h2>
+      <div className="controls">
+        <p className="info">MacbookPro 16" in Silver / Space Black</p>
+        <div className="flex-center gap-5 mt-5">
+          <div className="color-control">
+            <div
+              onClick={() => setColor("#abd5bd")}
+              className={clsx(
+                "bg-neutral-300",
+                color === "#abd5bd" && "active"
+              )}
+            />
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx(
+                "bg-neutral-900",
+                color === "#2e2c2e" && "active"
+              )}
+            />
+          </div>
+          <div className="size-control">
+            <div
+              onClick={() => setScale(0.06)}
+              className={clsx(
+                scale === 0.06
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
+              <p>14"</p>
+            </div>
+            <div
+              onClick={() => setScale(0.08)}
+              className={clsx(
+                scale === 0.08
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
+              <p>16"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-white text-4xl">Render Canvas</p>
+    </section>
+  );
+};
