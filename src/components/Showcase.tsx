038de6e4d20d34c1c387ec79b9cb20e6a33a484b
devsujalpatel <sujal7455@gmail.com>
2025-10-11 04:41:41 +0530

    Showcase Section Implemented

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,23 +1,57 @@
-import React from "react";
+"use client";
+
+import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const Showcase = () => {
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+
+  useGSAP(() => {
+    if (!isTablet) {
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#showcase",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+          pin: true,
+        },
+      });
+
+      timeline
+        .to(".mask img", {
+          scale: 1.1, // use numeric transform, not string
+          ease: "none",
+        })
+        .to(".content", {
+          opacity: 1,
+          y: 0,
+          ease: "ease",
+        });
+    }
+  }, [isTablet]);
+
   return (
     <section id="showcase">
-      <div className="media">
+      <div className="media relative">
         <video src="/videos/game.mp4" loop muted autoPlay playsInline />
-        <div className="mask">
+        <div className="mask absolute inset-0 flex items-center justify-center">
           <img src="/mask-logo.svg" />
         </div>
       </div>
 
-      <div className="content">
-        <div className="wrapper">
+      <div className="content opacity-0 translate-y-10 transition-all">
+        <div className="wrapper flex justify-between">
           <div className="lg:max-w-md">
             <h2>Rocket Chip</h2>
 
             <div className="space-y-5 mt-7 pe-10">
               <p>
-                Introducing
+                Introducing{" "}
                 <span className="text-white">
                   M4, the next generation of Apple silicon
                 </span>
@@ -39,6 +73,7 @@ export const Showcase = () => {
               </p>
             </div>
           </div>
+
           <div className="max-w-3xs space-y-14">
             <div className="space-y-2">
               <p>Up to</p>
