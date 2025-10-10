03592e28cd030f1044a2efcd549efb58e067f78f
devsujalpatel <sujal7455@gmail.com>
2025-10-11 04:17:14 +0530

    Feat:Showcase component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/Hero";
 import { NavBar } from "@/components/NavBar";
 import { ProductViewer } from "@/components/ProductViewer";
+import { Showcase } from "@/components/Showcase";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 }
